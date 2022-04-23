7477837c5bb77d518ae8560390f3b3ae1a3d329f
Omar Saad <62719848+Omar-Saad@users.noreply.github.com>
2022-04-24 00:57:38 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/omar/flutter/mvvm/flutter_clean_architecture/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.omar.flutter.mvvm.flutter_clean_architecture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/app.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+
+
+  MyApp._internal(){
+    print("MyApp._internal");
+
+  } // private constructor
+
+  static final  MyApp instance = MyApp._internal();  // singleton
+
+  factory MyApp()=> instance; // factory for class instance
+
+
+
+
+
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
